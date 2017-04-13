0090afe06ce4fd1bf5966ea10b17403b5f1506c7
Ben Wilcox <reccanti@gmail.com>
2017-04-12 22:09:39 -0400

    Add new parks to parks.json
    Adds more parks so that the total number is now 15. This also
    replaces the "no name found" park

--- ParkFinder/parks.js
+++ ParkFinder/parks.js
@@ -1,9 +1,79 @@
-{"parks":[
-{"latitude":"42.685","longitude":"-77.95944","name":"Letchworth State Park"},
-{"latitude":"43.36139","longitude":"-77.95861","name":"Hamlin Beach State Park"},
-{"latitude":"42.53111","longitude":"-77.88167","name":"Rattlesnake Hill State Wildlife Management Area"},
-{"latitude":"42.51667","longitude":"-77.69278","name":"Stony Brook State Park"},
-{"latitude":"43.30028","longitude":"-77.71","name":"Braddock Bay State Park"},
-{"latitude":"42.633201","longitude":"-77.36551","name":"High Tor Fish and Wildlife Management Area"},
-{"latitude":"42.0","longitude":"-77.0"}
-]}
+{
+    "parks": [
+              {
+                "latitude":"42.038990326799997",
+                "longitude":"-78.843833113100004",
+                "name":"Allegany State Park - Quaker Area"
+              },
+              {
+                "latitude":"43.168255",
+                "longitude":"-79.045052",
+                "name":"Artpark State Park (Earl W. Brydges)"
+              },
+              {
+                "latitude":"42.685",
+                "longitude":"-77.95944",
+                "name":"Letchworth State Park"
+              },
+              {
+                "latitude":"40.612817900499998",
+                "longitude":"-73.772552933300005",
+                "name":"Bayswater Point State Park"
+              },
+              {
+                "latitude":"44.200558",
+                "longitude":"-76.191666",
+                "name":"Cedar Point State Park"
+              },
+              {
+                "latitude":"44.332329",
+                "longitude":"-75.992378",
+                "name":"Dewolf Point State Park"
+              },
+              {
+                "latitude":"43.329971",
+                "longitude":"-76.697014",
+                "name":"Fair Haven Beach State Park"
+              },
+              {
+                "latitude":"40.750419",
+                "longitude":"-73.960583",
+                "name":"Franklin D. Roosevelt Four Freedoms State Park"
+              },
+              {
+                "latitude":"41.711",
+                "longitude":"-73.949",
+                "name":"Walkway Over the Hudson State Historic Park"
+              },
+              {
+                "latitude":"43.36139",
+                "longitude":"-77.95861",
+                "name":"Hamlin Beach State Park"
+              },
+              {
+                "latitude":"42.53111",
+                "longitude":"-77.88167",
+                "name":"Rattlesnake Hill State Wildlife Management Area"
+              },
+              {
+                "latitude":"42.51667",
+                "longitude":"-77.69278",
+                "name":"Stony Brook State Park"
+              },
+              {
+                "latitude":"43.30028",
+                "longitude":"-77.71",
+                "name":"Braddock Bay State Park"
+              },
+              {
+                "latitude":"42.633201",
+                "longitude":"-77.36551",
+                "name":"High Tor Fish and Wildlife Management Area"
+              },
+              {
+                "latitude":"42.420944",
+                "longitude":"-79.428612",
+                "name": "Lake Erie State Park"
+              }
+    ]
+}
