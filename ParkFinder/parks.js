0ea66bed66c9c054be14823482abd0c733080bbc
Ben Wilcox <reccanti@gmail.com>
2017-04-15 18:49:54 -0400

    Add descriptions to park data
    Adds descriptions to park data so that it can be added to the
    StatePark model and Annotations later

--- ParkFinder/parks.js
+++ ParkFinder/parks.js
@@ -4,90 +4,105 @@
                 "latitude":"42.038990326799997",
                 "longitude":"-78.843833113100004",
                 "name":"Allegany State Park - Quaker Area",
+                "description":"The Quaker area is known for its two lakes and wonderful camping experiences. It offers swimming, fishing, canoeing and kayaking, as well as hiking trails and playing fields.",
                 "url": "https://parks.ny.gov/parks/1/details.aspx"
               },
               {
                 "latitude":"43.168255",
                 "longitude":"-79.045052",
                 "name":"Artpark State Park (Earl W. Brydges)",
+                "description":"The Artpark is dedicated to presenting the finest in performing and visual arts programs. Experience the many concerts, plays, exhibits, classes and workshops offered here.",
                 "url": "https://parks.ny.gov/parks/113/details.aspx"
               },
               {
                 "latitude":"42.685",
                 "longitude":"-77.95944",
                 "name":"Letchworth State Park",
+                "description":"The \"Grand Canyon of the East,\" Letchworth is one of the most magnificent areas in the eastern U.S. Waterfalls, cliffs, and a deep gorge are just some of the parks stunning features.",
                 "url":"https://parks.ny.gov/parks/79/details.aspx"
               },
               {
                 "latitude":"40.612817900499998",
                 "longitude":"-73.772552933300005",
                 "name":"Bayswater Point State Park",
+                "description":"Located at the tip of a peninsula in the Rockaways the park offers varied terrain including beachfront, wetlands and woodlands making it an ideal location for nesting birds.",
                 "url": "https://parks.ny.gov/parks/86/details.aspx"
               },
               {
                 "latitude":"44.200558",
                 "longitude":"-76.191666",
                 "name":"Cedar Point State Park",
+                "description":"Cedar Point is one of the oldest state parks in New York. It is a popular spot, offering excellent camping, fishing, boating and swimming as well as stunning river views.",
                 "url": "https://parks.ny.gov/parks/21/details.aspx"
               },
               {
                 "latitude":"44.332329",
                 "longitude":"-75.992378",
                 "name":"Dewolf Point State Park",
+                "description":"A small, rustic park Dewolf Point is perfect for anglers and nature lovers. The park offers a cabin colony, camping area, stone gazebo and dockage for boats on the Lake of the Isles.",
                 "url": "https://parks.ny.gov/parks/22/details.aspx"
               },
               {
                 "latitude":"43.329971",
                 "longitude":"-76.697014",
                 "name":"Fair Haven Beach State Park",
+                "description": "Found on Lake Ontario the park boasts one of the finest public lakefronts in upstate New York. Towering shoreline bluffs, sandy beaches and hilly woodlands cover this scenic park.",
                 "url": "https://parks.ny.gov/parks/12/details.aspx"
               },
               {
                 "latitude":"40.750419",
                 "longitude":"-73.960583",
                 "name":"Franklin D. Roosevelt Four Freedoms State Park",
+                "description":"Franklin D. Roosevelt Four Freedoms Park is located on Roosevelt Island in New York City. Designed by architect Louis I. Kahn, the park is an inspiring civic space, and free to the public.",
                 "url": "https://parks.ny.gov/parks/186/details.aspx"
               },
               {
                 "latitude":"41.711",
                 "longitude":"-73.949",
                 "name":"Walkway Over the Hudson State Historic Park",
+                "description":"The park provides access to the Hudson River's breathtaking landscape. The bridge deck stands 212 feet above the river's surface and is 6,678 feet (1.28 miles) long, making it the longest, elevated pedestrian bridge in the world.",
                 "url": "https://parks.ny.gov/parks/178/details.aspx"
               },
               {
                 "latitude":"43.36139",
                 "longitude":"-77.95861",
                 "name":"Hamlin Beach State Park",
+                "description":"Clear water, sandy beaches and tent and trailer campsites entice thousands of visitors each year.",
                 "url": "https://parks.ny.gov/parks/20/details.aspx"
               },
               {
                 "latitude":"42.53111",
                 "longitude":"-77.88167",
                 "name":"Rattlesnake Hill State Wildlife Management Area",
+                "description":"The primary purpose of the Rattlesnake Hill WMA is for wildlife management, wildlife habitat management, and wildlife-dependent recreation",
                 "url": "http://www.dec.ny.gov/outdoor/24443.html"
               },
               {
                 "latitude":"42.51667",
                 "longitude":"-77.69278",
                 "name":"Stony Brook State Park",
+                "description":"Hilly woodlands, a deep gorge with rugged cliffs, three waterfalls and fascinating rock formations comprise the park offering camping, hiking and swimming in a stream-fed pool.",
                 "url": "https://parks.ny.gov/parks/118/details.aspx"
               },
               {
                 "latitude":"43.30028",
                 "longitude":"-77.71",
                 "name":"Braddock Bay State Park",
+                "description":"The primary purposes of the Braddock Bay WMA is for wildlife management, wildlife habitat management, and wildlife-dependent recreation",
                 "url": "http://www.dec.ny.gov/outdoor/24428.html"
               },
               {
                 "latitude":"42.633201",
                 "longitude":"-77.36551",
                 "name":"High Tor Fish and Wildlife Management Area",
+                "description":"The primary purposes of High Tor WMA is for wildlife management, wildlife habitat management, and wildlife-dependent recreation",
                 "url": "http://www.dec.ny.gov/outdoor/24439.html"
               },
               {
                 "latitude":"42.420944",
                 "longitude":"-79.428612",
                 "name": "Lake Erie State Park",
+                "description":"High bluffs overlooking Lake Erie provide a breathtaking view. Enjoy the scenic camping, swimming, and hiking opportunities as well as the excellent view of migrating birds.",
                 "url": "https://parks.ny.gov/parks/129/details.aspx"
               }
     ]
